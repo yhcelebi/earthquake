7f0788297ed9e3c87c835178098146ba834b7c8e
Yusuf Hamza Çelebi <yhcelebi@gmail.com>
2024-05-02 18:20:02 +0300

    resolved some dependency uncompatibilities

---
+++ services/earthquake-data-aggregator-service/src/scrapers/turkey/kandilli_scraper.py
@@ -0,0 +1,28 @@
+#scraper.py
+
+import requests
+import pandas as pd
+from bs4 import BeautifulSoup
+
+
+# URL of the webpage to scrape
+url = 'http://www.koeri.boun.edu.tr/scripts/lst9.asp'
+
+# Make an HTTP request to the webpage
+response = requests.get(url)
+
+# Check the status code of the response
+print("Status Code:", response.status_code)
+
+# Print the response headers
+print("Response Headers:", response.headers)
+
+if response.status_code == 200:
+    soup = BeautifulSoup(response.text, "html.parser")
+    content = soup.find("pre")
+    print(content)
+
+else:
+    print(response.status_code)
+
+
