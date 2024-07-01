dcea7864767ffd95634e0826164f629a8e6ac38f
Yusuf Hamza Çelebi <yhcelebi@gmail.com>
2024-07-01 17:50:03 +0300

    added some websocket code in fastapi in main.py for the EMSC connection to the main api powered by spring

--- services/earthquake-data-aggregator-service/src/main.py
+++ services/earthquake-data-aggregator-service/src/main.py
@@ -1,12 +1,32 @@
 # main.py
 
-from contextlib import asynccontextmanager
-from fastapi import FastAPI, HTTPException
-from typing import List
 from database import earthquakes_collection
 from models import Earthquake, EarthquakeCreate
 from uuid import uuid4
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+from typing import List
 
+
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections: List[WebSocket] = []
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.active_connections.append(websocket)
+
+    def disconnect(self, websocket: WebSocket):
+        self.active_connections.remove(websocket)
+
+    async def send_message(self, message: str, websocket: WebSocket):
+        await websocket.send_text(message)
+
+    async def broadcast(self, message: str):
+        for connection in self.active_connections:
+            await connection.send_text(message)
+
+
+manager = ConnectionManager()
 
 app = FastAPI()
 
@@ -23,6 +43,18 @@ async def root():
 
     # Check the content of the response
     print("Response content:", response.content)
+
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await manager.connect(websocket)
+    try:
+        while True:
+            data = await websocket.receive_text()
+            await manager.broadcast(f"Message: {data}")
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
+        await manager.broadcast("A client disconnected.")
 
 
 @app.get("/earthquakes/", response_model=List[Earthquake])
